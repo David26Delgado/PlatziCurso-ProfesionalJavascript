6373412421db4407cf28c0bd1a0a012df31ac3b6
David Delgado <dadch13.14@gmail.com>
2020-01-10 21:43:11 -0400

    IntersectionObserver

---
+++ assets/plugins/AutoPause.js
@@ -0,0 +1,51 @@
+/**
+ * Inicializando el objeto AutoPause.
+ *
+ */
+class AutoPause {
+  /**
+   * Constructor de la clase
+   */
+  constructor() {
+    this.threshold = 0.25;
+    this.handleIntersection = this.handleIntersection.bind(this);
+  }
+
+  /**
+   * Método para pausar el vídeo.
+   *
+   * @param {MediaPlayer} player El reproductor/player.
+   *
+   * @return {void}
+   */
+  run(player) {
+    this.player = player;
+
+    const observer = new IntersectionObserver(this.handleIntersection, {
+      threshold: this.threshold
+    });
+
+    observer.observe(this.player.media);
+  }
+
+  /**
+   * Método manejador de la intersección.
+   *
+   * @param {*} entries
+   *
+   * @returns {void}
+   */
+  handleIntersection(entries) {
+    const entry = entries[0];
+
+    const isVisible = entry.intersectionRatio >= this.threshold;
+
+    if (isVisible) {
+      this.player.play();
+    } else {
+      this.player.pause();
+    }
+  }
+}
+
+export default AutoPause;
